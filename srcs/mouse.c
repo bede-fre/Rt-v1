88b72b76c851324e813897edf50bd014ea198e48
Lucas GUILLEROT <lguiller@e1r5p13.42.fr>
2018-07-18 16:54:27 +0200

    ajout de l'affichage des leaks sur le clic souris

--- srcs/mouse.c
+++ srcs/mouse.c
@@ -6,7 +6,7 @@
 /*   By: bede-fre <bede-fre@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/06/26 15:05:31 by bede-fre          #+#    #+#             */
-/*   Updated: 2018/07/18 14:46:45 by bede-fre         ###   ########.fr       */
+/*   Updated: 2018/07/18 16:53:25 by lguiller         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,7 @@ int		ft_button_press(int button, int x, int y, t_all *all)
 {
 	if (button == L_CLIC)
 	{
+		system("leaks rtv1 | grep 'leaked bytes' | cut -c16- | cut -c-7");
 		all->test = 1;
 		ft_ray_tracing(all, x, y);
 		all->test = 0;
