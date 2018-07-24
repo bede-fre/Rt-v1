490b40f652c3c98ade46445804dfb7ab35b9c954
Lucas GUILLEROT <lguiller@e1r5p13.42.fr>
2018-07-24 14:15:41 +0200

    changement des equations du cylindre et du cone pour pouvoir les voir de face

--- srcs/equations.c
+++ srcs/equations.c
@@ -6,7 +6,7 @@
 /*   By: bede-fre <bede-fre@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/06/26 12:49:57 by bede-fre          #+#    #+#             */
-/*   Updated: 2018/07/20 11:52:23 by lguiller         ###   ########.fr       */
+/*   Updated: 2018/07/24 10:53:16 by lguiller         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,9 +19,9 @@ double	ft_sphere(t_all *all, t_scene *tp, t_coord_3d *uni, t_coord_3d *pos)
 	(void)all;
 	p.a = pow(uni->x, 2.0) + pow(uni->y, 2.0) +
 		pow(uni->z, 2.0);
-	p.b = (uni->x * (pos->x - (double)tp->px) + uni->y *
-		(pos->y - (double)tp->py) + uni->z *
-		(pos->z - (double)tp->pz)) * 2.0;
+	p.b = (uni->x * (pos->x - (double)tp->px) +
+		uni->y * (pos->y - (double)tp->py) +
+		uni->z * (pos->z - (double)tp->pz)) * 2.0;
 	p.c = (pow(pos->x - (double)tp->px, 2.0) +
 		pow(pos->y - (double)tp->py, 2.0) +
 		pow(pos->z - (double)tp->pz, 2.0)) -
@@ -62,9 +62,9 @@ double	ft_cone(t_all *all, t_scene *tp, t_coord_3d *uni, t_coord_3d *pos)
 	(void)all;
 	p.a = pow(uni->x, 2.0) + pow(uni->y, 2.0) -
 		pow(uni->z, 2.0);
-	p.b = (uni->x * (pos->x - (double)tp->px) + uni->y *
-		(pos->y - (double)tp->py) - uni->z *
-		(pos->z - (double)tp->pz)) * 2.0;
+	p.b = (uni->x * (pos->x - (double)tp->px) +
+		uni->y * (pos->y - (double)tp->py) -
+		uni->z * (pos->z - (double)tp->pz)) * 2.0;
 	p.c = (pow(pos->x - (double)tp->px, 2.0) +
 		pow(pos->y - (double)tp->py, 2.0) -
 		pow(pos->z - (double)tp->pz, 2.0));
@@ -73,23 +73,35 @@ double	ft_cone(t_all *all, t_scene *tp, t_coord_3d *uni, t_coord_3d *pos)
 		return (-1.0);
 	p.t1 = (-p.b + sqrt(p.d)) / (2.0 * p.a);
 	p.t2 = (-p.b - sqrt(p.d)) / (2.0 * p.a);
-	return ((p.t1 < p.t2) ? p.t1 : p.t2);
+	if (p.t1 < 0.0 && p.t2 < 0.0)
+		return (-1.0);
+	if (p.t1 >= 0.0 && p.t2 >= 0.0)
+		return ((p.t1 < p.t2) ? p.t1 : p.t2);
+	else
+		return ((p.t1 >= 0.0) ? p.t1 : p.t2);
 }
 
 double	ft_cylinder(t_all *all, t_scene *tp, t_coord_3d *uni, t_coord_3d *pos)
 {
+
 	t_equ	p;
 
 	(void)all;
-	p.a = pow(uni->x, 2.0) + pow(uni->y, 2.0);
-	p.b = 2.0 * (uni->x * (pos->x - (double)tp->px)
-		+ 2.0 * uni->y * (pos->y - (double)tp->py));
-	p.c = (pow(pos->x - (double)tp->px, 2.0)
-		+ pow(pos->y - (double)tp->py, 2.0) - pow((double)tp->p4, 2.0));
+	p.a = pow(uni->x, 2.0) + pow(uni->z, 2.0);
+	p.b = (uni->x * (pos->x - (double)tp->px) +
+		uni->z * (pos->z - (double)tp->pz)) * 2.0;
+	p.c = (pow(pos->x - (double)tp->px, 2.0) +
+		pow(pos->z - (double)tp->pz, 2.0)) -
+		pow((double)tp->p4, 2.0);
 	p.d = pow(p.b, 2.0) - 4.0 * p.a * p.c;
 	if (p.d < 0.0)
 		return (-1.0);
 	p.t1 = (-p.b + sqrt(p.d)) / (2.0 * p.a);
 	p.t2 = (-p.b - sqrt(p.d)) / (2.0 * p.a);
-	return ((p.t1 < p.t2) ? p.t1 : p.t2);
+	if (p.t1 < 0.0 && p.t2 < 0.0)
+		return (-1.0);
+	if (p.t1 >= 0.0 && p.t2 >= 0.0)
+		return ((p.t1 < p.t2) ? p.t1 : p.t2);
+	else
+		return ((p.t1 >= 0.0) ? p.t1 : p.t2);
 }
