aaf3c3fe96119ca14459594bc125c142d18e3a3e
bede-fre <bede-fre@student.42.fr>
2018-07-26 17:55:32 +0200

    Fix camera rotations and add new spheres scene

--- libvect/rotation_3d.c
+++ libvect/rotation_3d.c
@@ -6,7 +6,7 @@
 /*   By: lguiller <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/07/26 15:39:47 by lguiller          #+#    #+#             */
-/*   Updated: 2018/07/26 15:46:53 by lguiller         ###   ########.fr       */
+/*   Updated: 2018/07/26 16:43:18 by bede-fre         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,18 +14,33 @@
 
 void	ft_rot_x(t_mat3 *v, double a)
 {
-	v->y = v->y * cos(a) - v->z * sin(a);
-	v->z = v->y * sin(a) + v->z * cos(a);
+	double	y;
+	double	z;
+
+	y = v->y;
+	z = v->z;
+	v->y = y * cos(a) - z * sin(a);
+	v->z = y * sin(a) + z * cos(a);
 }
 
 void	ft_rot_y(t_mat3 *v, double a)
 {
-	v->x = v->x * cos(a) + v->z * sin(a);
-	v->z = -v->x * sin(a) + v->z * cos(a);
+	double	x;
+	double	z;
+
+	x = v->x;
+	z = v->z;
+	v->x = x * cos(a) + z * sin(a);
+	v->z = -x * sin(a) + z * cos(a);
 }
 
 void	ft_rot_z(t_mat3 *v, double a)
 {
-	v->x = v->x * cos(a) - v->y * sin(a);
-	v->y = v->x * sin(a) + v->y * cos(a);
+	double	x;
+	double	y;
+
+	x = v->x;
+	y = v->y;
+	v->x = x * cos(a) - y * sin(a);
+	v->y = x * sin(a) + y * cos(a);
 }
