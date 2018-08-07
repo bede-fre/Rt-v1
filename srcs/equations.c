f1cacf6f28837def326b5dd710a899667c0830de
Lucas GUILLEROT <lguiller@e1r5p13.42.fr>
2018-08-07 13:25:06 +0200

    correction des erreurs sur l'equation du cone

--- srcs/equations.c
+++ srcs/equations.c
@@ -6,7 +6,7 @@
 /*   By: bede-fre <bede-fre@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/06/26 12:49:57 by bede-fre          #+#    #+#             */
-/*   Updated: 2018/08/06 16:52:55 by lguiller         ###   ########.fr       */
+/*   Updated: 2018/08/07 13:22:00 by lguiller         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -48,7 +48,7 @@ double	ft_plane(t_scene *tp, t_mat3 *uni, t_mat3 *pos)
 		((tp->univect.x * uni->x)
 		+ (tp->univect.y * uni->y)
 		+ (tp->univect.z * uni->z)));
-	if (t != 0.0)
+	if (t != 0.0001)
 		return (t);
 	else
 		return (-1.0);
@@ -58,15 +58,13 @@ double	ft_cone(t_scene *tp, t_mat3 *uni, t_mat3 *pos)
 {
 	t_equ	p;
 
-	p.a = pow(uni->x, 2.0) -
-		pow(uni->y, 2.0) +
-		pow(uni->z, 2.0);
-	p.b = ((uni->x * (pos->x - (double)tp->px)) +
-		(uni->z * (pos->z - (double)tp->pz)) -
-		(uni->y * (pos->y - (double)tp->py) * pow(tan(ft_rad((double)tp->p4)), 2.0))) * 2.0;
-	p.c = ((pow(pos->x - (double)tp->px, 2.0)) +
-		(pow(pos->z - (double)tp->pz, 2.0)) -
-		(pow(pos->y - (double)tp->py, 2.0) * pow(tan(ft_rad((double)tp->p4)), 2.0)));
+	p.a = pow(uni->x, 2.0) - (pow(uni->y, 2.0) * tan(ft_rad((double)tp->p4))) + pow(uni->z, 2.0);
+	p.b = ((uni->x * (pos->x - (double)tp->px)) -
+		((uni->y * (pos->y - (double)tp->py)) * tan(ft_rad((double)tp->p4))) +
+		(uni->z * (pos->z - (double)tp->pz))) * 2.0;
+	p.c = (pow(pos->x - (double)tp->px, 2.0)) -
+		((pow(pos->y - (double)tp->py, 2.0)) * tan(ft_rad((double)tp->p4))) +
+		(pow(pos->z - (double)tp->pz, 2.0));
 	p.d = pow(p.b, 2.0) - 4.0 * p.a * p.c;
 	if (p.d < 0.0)
 		return (-1.0);
