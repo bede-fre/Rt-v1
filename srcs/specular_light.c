a1eac47c26a68e80fb89d998f1862d5bd724378b
Lynux2142 <lguiller@student.42.fr>
2018-09-07 18:13:59 +0200

    ++

--- srcs/specular_light.c
+++ srcs/specular_light.c
@@ -6,7 +6,7 @@
 /*   By: bede-fre <bede-fre@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/09/06 16:14:09 by bede-fre          #+#    #+#             */
-/*   Updated: 2018/09/07 15:05:16 by lguiller         ###   ########.fr       */
+/*   Updated: 2018/09/07 18:13:03 by lguiller         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,6 @@
 
 double	ft_specular_light(t_mat3 spot, t_mat3 inter, t_mat3 norm, t_mat3 cam)
 {
-	double	ret;
 	double	angle;
 	t_mat3	spot_int;
 	t_mat3	int_cam;
@@ -23,18 +22,15 @@ double	ft_specular_light(t_mat3 spot, t_mat3 inter, t_mat3 norm, t_mat3 cam)
 	spot_int.x = inter.x - spot.x;
 	spot_int.y = inter.y - spot.y;
 	spot_int.z = inter.z - spot.z;
+	spot_int = ft_normalize(spot_int);
 	int_cam.x = cam.x - inter.x;
 	int_cam.y = cam.y - inter.y;
 	int_cam.z = cam.z - inter.z;
+	int_cam = ft_normalize(int_cam);
 	int_ref.x = spot_int.x - 2.0 * norm.x * ft_dot_product(norm, spot_int);
 	int_ref.y = spot_int.y - 2.0 * norm.y * ft_dot_product(norm, spot_int);
 	int_ref.z = spot_int.z - 2.0 * norm.z * ft_dot_product(norm, spot_int);
-	angle = acos(ft_dot_product(int_ref, int_cam) / (ft_vect_dist(int_ref)
-		* (ft_vect_dist(int_cam)))) * 180.0 / M_PI;
-	if (angle < 20.0)
-		ret = 255.0 * pow(ft_dot_product(int_ref, int_cam) /
-			(ft_vect_dist(int_ref) * ft_vect_dist(int_cam)), 75.0);
-	else
-		ret = 0.0;
-	return (ret);
+	angle = ft_deg(acos(ft_dot_product(int_ref, int_cam)));
+	return ((angle < 20.0) ?
+		255.0 * pow(ft_dot_product(int_ref, int_cam), 75.0) : 0.0);
 }
